8eb98d0603262f78979cb74c39a1da1c6455631c
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-01-15 23:21:16 -0500

    Add async runtime setup for Tauri application

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,23 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use tauri::Manager;
+mod db;
+use db::setup_db;
+struct AppState {
+    db: db::Db,
 }
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
+        .setup(|app|{
+            tauri::async_runtime::block_on(async move {
+                let db = setup_db(&app).await;
+                app.manage(AppState{db});
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
