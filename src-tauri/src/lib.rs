e3e57226df787e06cabc91b3fd6fb1d4143d9b5e
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-01-16 22:13:34 -0500

    Refactor app state management and integrate todo commands in Tauri application

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,22 +1,24 @@
+mod commands;
 mod db;
 mod models;
-use tauri::Manager;
+mod store;
+use commands::todos::{delete_todo, get_todos, update_todo};
 use db::setup_db;
-
-struct AppState {
-    db: db::Db,
-}
-
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![])
-        .setup(|app|{
+        .invoke_handler(tauri::generate_handler![
+            get_todos,
+            update_todo,
+            delete_todo
+        ])
+        .setup(|app| {
             tauri::async_runtime::block_on(async move {
                 let db = setup_db(&app).await;
-                app.manage(AppState{db});
+                app.manage(store::AppState { db });
             });
             Ok(())
         })
