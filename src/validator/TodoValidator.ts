bea0f58175b48d1e79ca8fb8faf5c991ab552141
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-01-20 00:31:16 -0500

    Add TodoValidator class for validating todo items with class-validator

---
+++ src/validator/TodoValidator.ts
@@ -0,0 +1,18 @@
+import {IsEnum, IsString, Length } from "class-validator";
+import { TodoStatus } from "../models/TodoStatus";
+import { ValidateData } from "./ValidateData";
+
+export class TodoValidator{
+    @IsString({ message: "¡Ups! La descripción debe ser un texto." })
+    @Length(10, 50, { message: "La descripción debe tener entre 10 y 50 caracteres. ¡No te olvides!" })
+    description!: string;
+
+    @IsEnum(TodoStatus, { message: "El estado debe ser 'Incomplete' o 'Complete'." })
+    status!: TodoStatus;
+
+    async validate(object: object) {
+        const validator = new ValidateData();
+        return validator.init(object, TodoValidator);
+      }
+}
+
